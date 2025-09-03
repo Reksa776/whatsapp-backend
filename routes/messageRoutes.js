959d0f9eea139860c8f451a5b9af905978583a6b
Reksa776 <116544285+Reksa776@users.noreply.github.com>
2025-09-03 13:49:28 +0700

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,10 +3,12 @@ import multer from "multer";
 import { sendMessage } from "../controllers/messageController.js";
 
 const router = express.Router();
-const upload = multer({ dest: "storages/" });
+const upload = multer({ dest: "/tmp" });
+
 
 
 // Kirim pesan
 router.post("/sending", upload.single("file"), sendMessage);
 
 export default router;
+
